bdbd0a4e690d9738d476108530839ddf183ab5db
Matheus <matheus.santoslopes45@gmail.com>
2024-07-29 11:47:26 -0300

    V1 07-29

---
+++ app/src/main/java/com/example/tasks_mvvm/ui/main/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.tasks_mvvm.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.tasks_mvvm.data.model.Task
+import com.example.tasks_mvvm.data.repository.TaskRepository
+import com.example.tasks_mvvm.dto.TaskDto
+
+class MainViewModel: ViewModel() {
+
+    private val repository = TaskRepository()
+    private val _tasks = MutableLiveData<List<TaskDto>>()
+    val tasks: LiveData<List<TaskDto>> = _tasks
+
+    init {
+        loadData()
+    }
+
+    private fun loadData(){
+        val  taskList = repository.findAll()
+        val taskDtoList = taskList.map {
+            task: Task -> TaskDto(task.id, task.description, task.isCompleted)
+        }
+
+        _tasks.value = taskDtoList
+    }
+
+
+}
